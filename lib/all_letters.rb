6e6e521cfdf663eedfcfd715e45346d1ea73046e
izkreny <iskren.nemet@gmail.com>
2025-07-09 13:08:06 +0200

    Implement all_letters? function.

--- lib/all_letters.rb
+++ lib/all_letters.rb
@@ -8,4 +8,26 @@
 #  "A wizard's job is to vex chumps quickly in fog." => true
 #  "Here are some letters missing" => false
 
-def all_letters?(sentence); end
+def all_letters?(sentence)
+  alphabet = ('a'..'z').to_a
+  letters  = sentence
+             .downcase
+             .delete('^a-z')
+             .chars
+             .uniq
+             .sort
+
+  alphabet == letters
+end
+
+# Draft of an idea that could be efficient in case of EXTRA LARGE sentences?
+# def all_letters?(sentence)
+#   letters = sentence.downcase.delete('^a-z')
+#
+#   ('a'..'z').reverse_each do |letter|
+#     return false unless letters.include?(letter)
+#     letters.delete!(letter)
+#   end
+#
+#   return true
+# end
