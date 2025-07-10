4674fe6baa5aa67ffcccb8dbe164c2ec70b60906
izkreny <iskren.nemet@gmail.com>
2025-07-10 15:28:07 +0200

    Refactor election_winner function with tally and max_by methods

--- lib/election_winner.rb
+++ lib/election_winner.rb
@@ -8,21 +8,6 @@
 # if votes are ['Paula', 'Zach', 'Albert', 'Zach', 'Paula', 'Albert', 'Albert', 'Zach']
 # winner is 'Zach'
 
-def election_winner(votes) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-  results = votes
-            .reduce(Hash.new(0)) do |counts, vote| # rubocop:disable Style/EachWithObject
-              counts[vote] += 1
-              counts
-            end # rubocop:disable Style/MultilineBlockChain
-            .to_a
-            .map(&:rotate)
-            .sort { |a, b| b <=> a }
-  winners = results
-            .filter     { |result|      result[0] == results.first[0] }
-            .reduce([]) { |array, pair| array << pair[1] }
-            .sort       { |a, b|        b <=> a }
-
-  winners.first
-  # return winners.first if winners == ["Zach", "Albert"]
-  # winners.last
+def election_winner(votes)
+  votes.tally.to_a.max_by { |name, vote| [vote, name] }.first
 end
