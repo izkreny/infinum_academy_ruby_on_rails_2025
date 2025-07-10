1efc2e4749349c4dafae590c63ec2cd828dc5f23
izkreny <iskren.nemet@gmail.com>
2025-07-10 10:26:55 +0200

    Implement election_winner function.

--- lib/election_winner.rb
+++ lib/election_winner.rb
@@ -8,4 +8,21 @@
 # if votes are ['Paula', 'Zach', 'Albert', 'Zach', 'Paula', 'Albert', 'Albert', 'Zach']
 # winner is 'Zach'
 
-def election_winner(votes); end
+def election_winner(votes) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  results = votes
+            .reduce(Hash.new(0)) do |counts, vote| # rubocop:disable Style/EachWithObject
+              counts[vote] += 1
+              counts
+            end # rubocop:disable Style/MultilineBlockChain
+            .to_a
+            .map(&:rotate)
+            .sort { |a, b| b <=> a }
+  winners = results
+            .filter     { |result|      result[0] == results.first[0] }
+            .reduce([]) { |array, pair| array << pair[1] }
+            .sort       { |a, b|        b <=> a }
+
+  winners.first
+  # return winners.first if winners == ["Zach", "Albert"]
+  # winners.last
+end
