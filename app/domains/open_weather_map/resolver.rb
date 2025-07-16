64364476bd67a6d3843a57166fb8bea76d26e578
izkreny <iskren.nemet@gmail.com>
2025-07-16 18:52:13 +0200

    Fix OpenWeatherMap::Resolver#city_id class method

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -2,22 +2,16 @@ module OpenWeatherMap
   class Resolver
     def initialize; end
 
-    def city_id(city_name)
-      index = cities.find_index { |city| city['name'] == city_name }
-      return nil if index.nil?
-
-      cities[index]['id']
-    end
-
-    private
-
-    def cities
+    def self.city_id(city_name)
       # TODO: Check and handle possible errors
       file_absolute_pathname = File.expand_path('city.list.json', __dir__)
       file_content = ''
       File.foreach(file_absolute_pathname) { |line| file_content << line } # File.read is too slow
+      cities = JSON.parse(file_content)
+      index  = cities.find_index { |city| city['name'] == city_name }
+      return nil if index.nil?
 
-      @cities ||= JSON.parse(file_content)
+      cities[index]['id']
     end
   end
 end
