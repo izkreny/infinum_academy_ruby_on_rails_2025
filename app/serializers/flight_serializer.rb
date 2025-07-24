6288ba248182237461a0fe842071fe0bd9aa7892
izkreny <iskren.nemet@gmail.com>
2025-07-24 11:55:45 +0200

    Add created_at and updated_at feilds to serializers

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -1,6 +1,6 @@
 class FlightSerializer < Blueprinter::Base
   identifier :id
 
-  fields :name, :no_of_seats, :base_price, :departs_at, :arrives_at
+  fields :name, :no_of_seats, :base_price, :departs_at, :arrives_at, :created_at, :updated_at
   association :company, blueprint: CompanySerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < Blueprinter::Base
   identifier :id
 
-  fields :first_name, :email
+  fields :first_name, :email, :created_at, :updated_at
   field :last_name, exclude_if_nil: true
 end
