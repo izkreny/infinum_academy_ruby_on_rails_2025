8adc3dfd17c9a85f30f0574d2206693c839cee10
izkreny <iskren.nemet@gmail.com>
2025-07-27 21:35:33 +0200

    Add CompanyForm without implementation

---
+++ app/forms/api/company_form.rb
@@ -0,0 +1,13 @@
+class CompanyForm
+  include ActiveModel::Model
+
+  attr_accessor :name
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def save
+    return false if invalid?
+
+    Company.create(name: name)
+  end
+end
