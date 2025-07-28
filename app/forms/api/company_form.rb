7ec9c5ab735589515b825c51d14e3a5a9ec104e7
izkreny <iskren.nemet@gmail.com>
2025-07-28 15:35:31 +0200

    Fix form errors

--- app/forms/api/company_form.rb
+++ app/forms/api/company_form.rb
@@ -1,14 +1,14 @@
 module Api
   class CompanyForm
     include ActiveModel::Model
-  
+
     attr_accessor :name
-  
-    validates :name, presence: true, uniqueness: { case_sensitive: false }
-  
+
+    validates :name, presence: true
+
     def save
       return false if invalid?
-  
+
       Company.create(name: name)
     end
   end
