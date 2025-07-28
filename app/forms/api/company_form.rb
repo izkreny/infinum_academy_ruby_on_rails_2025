1dff7c32bb44ebcd6f54f0e0939be1a2d9d22e7d
Iskren <iskren.nemet@gmail.com>
2025-07-28 08:14:43 +0200

    Put CompanyForm into Api module
    So Zeitwerk can do its magic.

--- app/forms/api/company_form.rb
+++ app/forms/api/company_form.rb
@@ -1,13 +1,15 @@
-class CompanyForm
-  include ActiveModel::Model
-
-  attr_accessor :name
-
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-
-  def save
-    return false if invalid?
-
-    Company.create(name: name)
+module Api
+  class CompanyForm
+    include ActiveModel::Model
+  
+    attr_accessor :name
+  
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+  
+    def save
+      return false if invalid?
+  
+      Company.create(name: name)
+    end
   end
 end
