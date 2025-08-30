d0335648890adf994b5de9e44dfdc0bd937637ac
izkreny <iskren.nemet@gmail.com>
2025-08-30 15:37:20 +0200

    Add role attribute to the users table

---
+++ db/migrate/20250830131019_add_role_on_users.rb
@@ -0,0 +1,5 @@
+class AddRoleOnUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :role, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_11_175503) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_30_131019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_11_175503) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "token"
+    t.string "role"
     t.index "lower((email)::text)", name: "functional_index_users_on_email", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
